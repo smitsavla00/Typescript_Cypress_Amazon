195a40913aa03770ad075a63617c9a2bb052e009
Smit <Smitsavla00@gmail.com>
2026-02-11 16:45:51 +0530

    Updated file greythr files

--- cypress/e2e/pages/GreythrSignIn.ts
+++ cypress/e2e/pages/GreythrSignIn.ts
@@ -1,28 +1,28 @@
-class GreythrIn {
-  elements = {
-    logo: () => cy.get('.gt-greytip-header-logo'),
-    email_input: () => cy.get('#username'),
-    // email_continue: () => cy.get('#continue'),
-    password_input: () => cy.get('#password'),
-    login: () => cy.contains('button', 'Login'),
-    signin_btn: () => cy.contains('button', 'Sign In'),
-    sign_out_btn: () => cy.contains('button', 'Sign Out'),
-  }
+// class GreythrIn {
+//   elements = {
+//     logo: () => cy.get('.gt-greytip-header-logo'),
+//     email_input: () => cy.get('#username'),
+//     // email_continue: () => cy.get('#continue'),
+//     password_input: () => cy.get('#password'),
+//     login: () => cy.contains('button', 'Login'),
+//     signin_btn: () => cy.contains('button', 'Sign In'),
+//     sign_out_btn: () => cy.contains('button', 'Sign Out'),
+//   }
 
-  visit() {
-    cy.visit('https://pipin.greythr.com/')
-  }
+//   visit() {
+//     cy.visit('https://pipin.greythr.com/')
+//   }
 
-  SignIn() {
-    this.elements.logo().should('be.visible')
-    this.elements.email_input().type('P282') // Enter your email
-    // this.elements.email_continue().click()
-    this.elements.password_input().type('Pippin@123') // Enter your password
-    this.elements.login().click()
-    cy.wait(3000)
-    cy.contains('Sign In', { includeShadowDom: true }).click()
-  }
-}
+//   SignIn() {
+//     this.elements.logo().should('be.visible')
+//     this.elements.email_input().type('') // Enter your email
+//     // this.elements.email_continue().click()
+//     this.elements.password_input().type('') // Enter your password
+//     this.elements.login().click()
+//     cy.wait(3000)
+//     cy.contains('Sign In', { includeShadowDom: true }).click()
+//   }
+// }
 
-// ✅ Correct default export
-export default new GreythrIn()
+// // ✅ Correct default export
+// export default new GreythrIn()
